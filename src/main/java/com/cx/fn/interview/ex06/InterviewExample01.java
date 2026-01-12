6ec64bfbd7d6e3845a2b46187f3977d49b24fd55
rahulshivsharan <rahulshivsharan@gmail.com>
2026-01-12 21:51:27 +0530

    Show numbers according to its occurance in list
    	for a given list of numbers, find for how many
    	times the numbers occurred,
    
    	output should be the number and the count of number occured
    	the output should be in descending order of occurance of number
    
    	This is implemented using Set

--- src/main/java/com/cx/fn/interview/ex06/InterviewExample01.java
+++ src/main/java/com/cx/fn/interview/ex06/InterviewExample01.java
@@ -1,9 +1,13 @@
 package com.cx.fn.interview.ex06;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -113,12 +117,66 @@ public class InterviewExample01 {
 		});
 	}
 	
+	/**
+	 * To count the number of times the numbers occurring 
+	 * and return a key value pair where key is number and value is number of
+	 * times it occurred
+	 * @throws Exception
+	 */
+	private static void getNumberGroupSet01() throws Exception{
+		List<Integer> numList = Arrays.asList(23, 45, 23, 66, 88, 66, 35);
+		
+		numList.stream().collect(Collectors.groupingBy((num) -> num, Collectors.counting())).entrySet().forEach((entryObject) -> {			
+			System.out.println("Number "+entryObject.getKey()+", occuring "+entryObject.getValue());
+		});
+	}	
+	
+	
+	
+	/**
+	 * To count the number of times the numbers occurring in number list
+	 * and return a key value pair sorted by value in descending order
+	 * 
+	 * times it occurred
+	 * @throws Exception
+	 */
+	private static void getNumberGroupSet03() throws Exception{
+		List<Integer> numList = Arrays.asList(23, 45, 23, 66, 88, 66, 35, 23);
+		
+		Function<Integer, Integer> fn = new Function<Integer, Integer>() {
+			
+			@Override
+			public Integer apply(Integer num) {				
+				return num;
+			}
+		};
+		
+		Comparator<Map.Entry<Integer, Long>> comparatorFn = new Comparator<Map.Entry<Integer,Long>>() {
+			
+			@Override
+			public int compare(Entry<Integer, Long> entry1, Entry<Integer, Long> entry2) {				
+				return entry2.getValue().compareTo(entry1.getValue());
+			}
+		};
+		
+		Set<Map.Entry<Integer, Long>> setOfNumbers = numList.stream()
+														.collect(Collectors.groupingBy(fn, Collectors.counting()))
+														.entrySet().stream().sorted(comparatorFn).collect(Collectors.toCollection(LinkedHashSet::new));
+		
+		setOfNumbers.forEach((entryObject) -> {
+			System.out.println("Number "+entryObject.getKey()+", occuring "+entryObject.getValue());
+		});
+	}
+	
+	
 	public static void main(String[] args) {
 		try {
 			//getNumberOccuranceCount01();
 			//getNumberOccuranceCount02();
 			//getNumberOccuranceCount03();
-			getNumberOccuranceCount04();
+			//getNumberOccuranceCount04();
+			//getNumberGroupSet01();
+			getNumberGroupSet03();
 		}catch(Exception e) {
 			System.out.println(e);
 		}
